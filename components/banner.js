b0ae4c87436db4cef1dfc9ee2c4b10f455d51aaa
Raghav <raghavbhat02@gmail.com>
2021-05-22 22:01:37 -0400

    Mobile responsiveness re-added, changed font-size, icon size

--- components/banner.js
+++ components/banner.js
@@ -4,10 +4,10 @@ const Banner = ({ headline, tagline }) => (
   <div className="flex flex-wrap justify-center bg-gradient-to-r from-gray-200 to-white">
   <div className="p-3 w-10/12 lg:w-2/3 bg-gradient-to-r from-gray-700 via-gray-900 to-gray-800 flex justify-start my-2 md:my-2"> 
     <div className=""></div>
-    <img className="w-12 h-12 md:w-auto md:h-auto" src= {"/rocket.png"}></img>
-    <div className="flex flex-wrap justify-left md:justify-center mx-5 md:mx-20">
+    <img className="w-12 h-12 md:w-24 md:h-24" src= {"/rocket.png"}></img>
+    <div className="flex flex-wrap justify-start mx-1 md:mx-2">
       <h1 className="font-bold text-2xl md:text-4xl text-gray-100 font-logo"> { headline } </h1>
-      <span className="hidden md:block text-gray-100 font-family font-bold font-logo"> Join us this summer for a sprint to create world-class products that solve real world problems</span>
+      <span className="text-gray-100 text-base md:text-xl font-family font-bold font-logo">{tagline} </span>
     </div>
   </div>
   </div>
@@ -15,14 +15,15 @@ const Banner = ({ headline, tagline }) => (
 
 const ProductStudioBanner = () => (
   <div>
-    { <Link href="https://studio.v1michigan.com" target="_blank">
-      <div className="cursor-pointer">
+    { /*<Link href="https://studio.v1michigan.com" target="_blank">*/ }
+      <div className="cursor-pointer"> 
       <Banner 
       headline="Introducing Product Studio"
-      tagline="Not really sure what to put here">
+      tagline="Join us this summer for a sprint to create world-class products that solve real world problems">
       </Banner>
       </div>
-  </Link> }
+  { /*</Link> */}
+ 
   </div>
   
 )
