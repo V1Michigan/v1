f3ed67c6a7f50ea92f6ccb97ab4bc9526343c0ec
Raghav <raghavbhat02@gmail.com>
2021-05-25 16:33:27 -0400

    changed banner colors to yellow to suit site theme

--- components/banner.js
+++ components/banner.js
@@ -2,24 +2,24 @@ import { ContentHeader, ContentBody } from "./content";
 import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline'
 
 import Link from "next/link";
-const Banner = ({ headline, tagline }) => (
-  <div className="bg-indigo-600">
+const Banner = ({ largeLine, smallLine, link }) => (
+  <div className="bg-yellow-600">
   <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
     <div className="flex items-center justify-between flex-wrap">
       <div className="w-0 flex-1 flex items-center">
-        <span className="flex p-2 rounded-lg bg-indigo-800">
+        <span className="flex p-2 rounded-lg bg-yellow-800">
           { /* <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" /> */ }
           <img className="h-6 w-6" src="/rocket_icon.png"></img>
         </span>
         <p className="ml-3 font-medium font-sans text-white truncate">
-          <span className="md:hidden">We announced a new product!</span>
-          <span className="hidden md:inline">Big news! We're excited to announce a brand new product.</span>
+          <span className="md:hidden"> { smallLine }</span>
+          <span className="hidden md:inline"> { largeLine }</span>
         </p>
       </div>
       <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
         <a
-          href="https://studio.v1michigan.com"
-          className="font-sans flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+          href= { link }
+          className="font-sans flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-yellow-600 bg-white hover:bg-indigo-50"
         >
           Learn more
         </a>
@@ -27,7 +27,7 @@ const Banner = ({ headline, tagline }) => (
       <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
         <button
           type="button"
-          className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+          className="-mr-1 flex p-2 rounded-md hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
         >
           <span className="sr-only">Dismiss</span>
           <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
@@ -39,17 +39,14 @@ const Banner = ({ headline, tagline }) => (
 )
 
 const ProductStudioBanner = () => (
-  <div className="cursor-pointer">
-     <Link href="https://studio.v1michigan.com" target="_blank">
+     
 
       <Banner 
-      headline="Introducing Product Studio"
-      tagline="Join us this summer for a sprint to create world-class products that solve real world problems">
+      largeLine="Blast off! Product Studio is Here! Join us this summer for a sprint to create world-class products that solve real world problems!"
+      smallLine="Product Studio is Here!"
+      link="https://studio.v1michigan.com">
       </Banner>
-      
-  </Link>
- 
-  </div>
+
   
 )
 
