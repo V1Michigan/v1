55e74dc9e4bbced4a0e0e7a443c608a850bcf915
samay <samay@umich.edu>
2021-06-18 22:50:03 -0400

    updates

--- components/calendarapi.js
+++ components/calendarapi.js
@@ -6,67 +6,41 @@ async function getData() {
     "https://damp-depths-59602.herokuapp.com/https://v1api-production.up.railway.app/events/"
   );
   const data = response.json();
-  console.log("hello");
-  console.log(data);
+
   return data;
 }
 
 const Calendar = () => {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    getData().then(
-      (data) => {
-        setIsLoaded(true), setEvents(data);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
-    );
+    getData().then((data) => {
+      setEvents(data);
+    });
   }, []);
 
-  if (error) {
-    return (
-      <div className="bg-gradient-to-r from-gray-200 to-white">
-        {" "}
-        Error: {error.message}
-      </div>
-    );
-  } else if (!isLoaded) {
-    return (
-      <div className="bg-gradient-to-r from-gray-200 to-white">Loading...</div>
-    );
-  } else if (!events) {
-    return (
-      <div className="bg-gradient-to-r from-gray-200 to-white">
-        No upcoming events.
-      </div>
-    );
-  } else {
-    return (
-      <ContentPage
-        title="Events"
-        textElement={
-          <div className="mx-auto ">
-            {events.map((event) => (
-              <div className="text-gray-900 bg-gray-200 p-4 m-4 rounded-md">
-                <h2 className="text-xl font-bold">{event.name}</h2>
-                <p className="text-sm text-gray-700">
-                  {new Date(event.start).toLocaleString()}
-                </p>
-                {/* <a href="location">Join here</a> */}
-
-                {/* <p>{ReactHtmlParser(event.description)}</p> */}
-              </div>
-            ))}
-          </div>
-        }
-      />
-    );
-  }
+  return (
+    <>
+      {events && events.length > 0 && (
+        <ContentPage
+          title="Upcoming Events"
+          textElement={
+            <div className="mx-auto">
+              {events.map((event) => (
+                <div className="w-full text-gray-900 bg-gray-200 p-4 m-4 rounded-md">
+                  <h2 className="text-xl font-bold">{event.name}</h2>
+                  <p className="text-sm text-gray-700">
+                    {new Date(event.start).toLocaleString()}
+                  </p>
+                  <p>{ReactHtmlParser(event.description)}</p>
+                </div>
+              ))}
+            </div>
+          }
+        />
+      )}
+    </>
+  );
 };
 
 export { Calendar };
