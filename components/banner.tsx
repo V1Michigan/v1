9a7e6f37686100d9ba042cdeab7dfb204c613d0e
Advait <advaitiyer03@gmail.com>
2022-02-22 18:44:35 -0500

    chore: migrate banners to ts

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,33 +1,34 @@
 import { XIcon } from '@heroicons/react/outline';
-import PropTypes from 'prop-types';
+import PropTypes, { ReactComponentLike } from 'prop-types';
+import { ReactElement } from 'react';
 
-const Banner = ({ largeLine, smallLine, link } ) => (
+const Banner = ({ largeLine, smallLine, link }: { largeLine: any, smallLine: any, link: any }) => (
   <div className="bg-yellow-600 full_banner">
     <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between flex-wrap">
         <div className="w-0 flex-1 flex items-center">
           <span className="flex p-2 rounded-lg bg-yellow-800">
-            { /* <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" /> */ }
+            { /* <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" /> */}
             <img className="h-6 w-6" src="/rocket_icon.png" alt="" />
           </span>
           <p className="ml-3 font-medium font-sans text-white truncate">
             <span className="md:hidden">
               {' '}
-              { smallLine }
+              {smallLine}
             </span>
             <span className="hidden md:inline">
               {' '}
-              { largeLine }
+              {largeLine}
             </span>
           </p>
         </div>
         <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
           <a
-            href={ link }
+            href={link}
             target="_blank"
             className="font-sans flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-yellow-600 bg-white hover:bg-yellow-50"
             rel="noreferrer"
-        >
+          >
             Learn more
           </a>
         </div>
@@ -35,10 +36,10 @@ const Banner = ({ largeLine, smallLine, link } ) => (
           <button
             type="button"
             className="-mr-1 flex p-2 rounded-md hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-            onClick={ () => {
+            onClick={() => {
               document.querySelector<HTMLElement>('.full_banner').style.display = 'none';
-            } }
-        >
+            }}
+          >
             <span className="sr-only">Dismiss</span>
             <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
           </button>
@@ -49,9 +50,9 @@ const Banner = ({ largeLine, smallLine, link } ) => (
 );
 
 Banner.propTypes = {
-  largeLine: PropTypes.elementType.isRequired,
-  smallLine: PropTypes.elementType.isRequired,
-  link: PropTypes.string.isRequired,
+  largeLine: PropTypes.element || PropTypes.string,
+  smallLine: PropTypes.element || PropTypes.string,
+  link: PropTypes.element || PropTypes.string,
 };
 
 const ProductStudioBanner = () => (
@@ -64,7 +65,7 @@ const ProductStudioBanner = () => (
 
 const StartupFairBanner = () => (
   <Banner
-    largeLine={ (
+    largeLine={(
       <>
         Apply by
         {' '}
@@ -75,7 +76,7 @@ const StartupFairBanner = () => (
         {' '}
         <span className="font-bold">V1 Startup Fair</span>
       </>
-    ) }
+    )}
     smallLine="Meet top startups at V1 Startup Fair!"
     link="https://startupfair.v1michigan.com"
   />
