b712130939f09d169b74dc471b1ed305d7bf7767
Advait <advaitiyer03@gmail.com>
2022-03-03 23:28:08 -0500

    added UI to the main sign up page. ran lint check

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import {
   Formik, Form, Field, ErrorMessage, FormikErrors,
 } from "formik";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import useSupabase from "../hooks/useSupabase";
 import { HOSTNAME } from "../pages/_app";
@@ -17,6 +18,7 @@ const REDIRECT_URL = `${HOSTNAME}/welcome`;
 
 export default function SignUp() {
   const { signIn, signUp } = useSupabase();
+  const router = useRouter();
 
   const [loading, setLoading] = useState(false);
   const [submitMessage, setSubmitMessage] = useState<string | null>(null);
@@ -39,8 +41,17 @@ export default function SignUp() {
   };
 
   return (
-    <div className="row flex flex-center">
-      <div className="col-6 form-widget">
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img
+            className="mx-auto h-20 w-auto"
+            src="https://v1michigan.com/favicon.ico?v=1"
+            // need to add a new image, this is obviously temporary
+            alt="Workflow"
+          />
+          <h3 className="mt-6 text-center text-2xl font-medium text-gray-900">Sign up now—we love new faces!</h3>
+        </div>
         <Formik
           initialValues={ {
             email: "",
@@ -76,16 +87,17 @@ export default function SignUp() {
           } }
           onSubmit={ async (values, { setSubmitting }) => {
             setLoading(true);
-            const { error } = await signUp(
+            const { user: existingUser, session, error } = await signUp(
               { email: values.email, password: values.password },
               { redirectTo: REDIRECT_URL },
             );
-            // A `user` is always returned, but will have a fake UUID +
-            // timestamps if the user already exists. We can't tell the difference
-            // though, so I don't think we can check for the user's existence here :/
-            // https://supabase.com/docs/reference/javascript/auth-signup#notes:
-            // https://github.com/supabase/supabase-js/issues/296
-            if (error) {
+            if (existingUser && session) {
+              // From https://supabase.com/docs/reference/javascript/auth-signup#notes:
+              //   New users: a user is returned but session will be null
+              //   Existing users: an obfuscated / fake user object will be returned.
+              setSubmitError("Found an existing user...redirecting to /login");
+              router.push("/login");
+            } else if (error) {
               setSubmitError(error.message);
             } else {
               // Per https://github.com/supabase/supabase/discussions/3526,
@@ -97,17 +109,21 @@ export default function SignUp() {
           } }
         >
           {({ errors, isSubmitting }) => (
-            <Form className="flex flex-col w-1/2 gap-y-4">
-              <div>
+            <Form className="mt-8 space-y-6" action="#" method="POST">
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
                 <Field
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   type="email"
                   name="email"
                   placeholder="billymagic@umich.edu"
-                  autoComplete="email" />
+                  autoComplete="email"
+                />
                 <ErrorMessage name="email" component="p" className="text-red-500" />
               </div>
               <div>
                 <Field
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   type="password"
                   name="password"
                   placeholder="Password"
@@ -116,13 +132,17 @@ export default function SignUp() {
               </div>
               <div>
                 <Field
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   type="password"
                   name="passwordConfirm"
                   placeholder="Confirm password"
                   autoComplete="new-password" />
                 <ErrorMessage name="passwordConfirm" component="p" className="text-red-500" />
               </div>
-              <button type="submit" disabled={ isSubmitting || Object.keys(errors).length > 0 }>
+              <button
+                type="submit"
+                disabled={ isSubmitting || Object.keys(errors).length > 0 }
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 {isSubmitting ? "Loading..." : "Let's go ›"}
               </button>
               {submitError && <p className="text-red-500">{submitError}</p>}
@@ -130,12 +150,14 @@ export default function SignUp() {
             </Form>
           )}
         </Formik>
-        <GoogleSignIn
-          onClick={ handleGoogleSignup }
-          disabled={ loading }
-        />
+        <div className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 data-width:300 data-height:400 data-longtitle:true">
+          <GoogleSignIn
+            onClick={ handleGoogleSignup }
+            disabled={ loading }
+          />
+        </div>
         <Link href="/login" passHref>
-          <p className="link">Already have an account? Log in</p>
+          <p className="group relative w-full flex justify-center font-medium text-indigo-600 hover:text-indigo-500 ">Already have an account? Log in</p>
         </Link>
       </div>
     </div>
