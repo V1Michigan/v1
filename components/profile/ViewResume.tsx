7b534b42f064b6fe24834abbba0474fc46561ab1
Elliot Klein <elliotfklein@gmail.com>
2022-03-08 09:54:42 -0500

    feat: handle resume load errors

--- components/profile/ViewResume.tsx
+++ components/profile/ViewResume.tsx
@@ -10,25 +10,34 @@ interface ViewResumeProps {
 }
 
 const ViewResume = ({ resume: resume_, maxPages = Infinity }: ViewResumeProps) => {
+  const [numPages, setNumPages] = useState<number>(0);
+  const [error, setError] = useState(false);
   const resumeUrl = useMemo(() => (typeof resume_ === "string" ? resume_ : URL.createObjectURL(resume_)),
     [resume_]);
-  const [numPages, setNumPages] = useState<number>(0);
-  return (
-    <a href={ resumeUrl } target="_blank" rel="noopener noreferrer">
-      <Document
-        file={ resumeUrl }
-        onLoadSuccess={ ({ numPages: pages }) => setNumPages(pages) }
+  const resumeContent = (
+    <Document
+      file={ resumeUrl }
+      onLoadSuccess={ ({ numPages: pages }) => setNumPages(pages) }
+      onLoadError={ () => setError(true) }
+      error="Error loading resume"
     >
-        <div className="flex flex-row overflow-x-auto">
-          {Array.from({ length: Math.min(numPages, maxPages) })
-            .map((_, n) => n + 1)
-            .map((n) => (
-              <Page key={ n } pageNumber={ n } height={ 300 } />
-            ))}
-        </div>
-      </Document>
-    </a>
+      <div className="flex flex-row overflow-x-auto">
+        {Array.from({ length: Math.min(numPages, maxPages) })
+          .map((_, n) => n + 1)
+          .map((n) => (
+            <Page key={ n } pageNumber={ n } height={ 300 } />
+          ))}
+      </div>
+    </Document>
   );
+  // Only link if there's not an error
+  return error
+    ? resumeContent
+    : (
+      <a href={ resumeUrl } target="_blank" rel="noopener noreferrer">
+        {resumeContent}
+      </a>
+    );
 };
 
 export default ViewResume;
