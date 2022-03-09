816b0684e3ae62b7cdd34157b646de3f8003ac96
Elliot Klein <elliotfklein@gmail.com>
2022-03-08 20:58:32 -0500

    fix: react-pdf resolution error during build

--- next.config.js
+++ next.config.js
@@ -5,5 +5,14 @@ module.exports = {
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
     },
+    webpack(config, { isServer }) {
+      // Don't load react-pdf code on the server, since react-pdf requires e.g. canvas
+      // https://github.com/wojtekmaj/react-pdf/issues/799
+      // https://github.com/mozilla/pdf.js/issues/13373
+      if (isServer) {
+        config.resolve.alias.canvas = false;
+      }
+      return config;
+    },
   };
 
