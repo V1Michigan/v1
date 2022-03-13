f15bfee0d9ec273c39b30027a37c7a043dd2ceac
Elliot Klein <elliotfklein@gmail.com>
2022-03-12 19:14:30 -0500

    fix: isObjectEqual type errors

--- utils/isObjectEqual.ts
+++ utils/isObjectEqual.ts
@@ -1,13 +1,14 @@
-/* eslint-disable */
 // Simple isObjectEqual function...could use lodash or underscore
-export default function isObjectEqual(a: object, b: object) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type MaybeEqualObject = { [key: string]: any };
+export default function isObjectEqual(a: MaybeEqualObject, b: MaybeEqualObject) {
   const keys = Object.keys(a);
   if (keys.length !== Object.keys(b).length) return false;
 
   for (let i = 0; i < keys.length; i += 1) {
     const key = keys[i];
     if (typeof a[key] === "object" && typeof b[key] === "object") {
-      if (!isObjectEqual(a[key], b[key])) return false;
+      if (!isObjectEqual(a[key] as MaybeEqualObject, b[key] as MaybeEqualObject)) return false;
     } else if (a[key] !== b[key]) {
       return false;
     }
