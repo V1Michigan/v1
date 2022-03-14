1edf35f866d427d983a7d0992ef947d2cb49652d
Elliot Klein <elliotfklein@gmail.com>
2022-03-14 12:31:54 -0400

    feat: align Step2Prompt button to bottom of container

--- components/Step2Prompt.tsx
+++ components/Step2Prompt.tsx
@@ -8,21 +8,23 @@ const Step2Prompt = () => {
     return null;
   }
   return (
-    <div className="bg-gray-100 max-w-sm rounded-md p-4 text-center">
+    <div className="bg-gray-100 max-w-sm rounded-md p-4 text-center flex flex-col">
       <h1 className="font-bold tracking-tight text-xl text-gray-900 mb-2">
         Finish filling out your profile
       </h1>
       <h2 className="text-gray-800">
         Tell us more about you and what you&apos;re excited about
       </h2>
-      <Link href="/welcome" passHref>
-        <button
-          className="bg-gradient-to-r from-blue-600 to-blue-700 hover:bg-blue-500 text-gray-100 font-semibold py-3 px-4 rounded shadow mt-3 hover:opacity-75"
-          type="button"
-        >
-          Let&apos;s go &rsaquo;
-        </button>
-      </Link>
+      <div className="mt-auto">
+        <Link href="/welcome" passHref>
+          <button
+            className="bg-gradient-to-r from-blue-600 to-blue-700 hover:bg-blue-500 text-gray-100 font-semibold py-3 px-4 rounded shadow hover:opacity-75"
+            type="button"
+          >
+            Let&apos;s go &rsaquo;
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
