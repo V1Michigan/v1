67fcc73f636b6ef6dc70fdc51c52738e6b766505
Elliot Klein <elliotfklein@gmail.com>
2022-03-13 23:32:13 -0400

    fix: lint

--- components/profile/EditProfile.tsx
+++ components/profile/EditProfile.tsx
@@ -10,7 +10,7 @@ interface EditProfileProps {
 
 const EditProfile = ({ profile }: EditProfileProps) => (
   <div className="grid grid-cols-2 gap-x-10 gap-y-2 justify-center items-center pl-10 pr-10">
-    {/* <EmailField value={ profile.email } label="Email" /> */}
+    <EmailField value={ profile.email } label="Email" />
     <PhoneField label="Phone" />
     <YearField label="School year" />
     <LinkedInField label="LinkedIn" />
