3917bd5d8e73c2c66bf092fa54c687cc02831006
Elliot Klein <elliotfklein@gmail.com>
2022-03-16 23:28:56 -0400

    feat: add 'Sign up' button to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Head from "next/head";
 import { What, Offer, Join } from "../components/about";
 import NavbarBuilder from "../components/NavBar";
@@ -60,17 +61,27 @@ export default function IndexPage() {
   </Link> */}
           <div className="md:flex">
             <div className="flex-1 p-8 flex items-center">
-              <div className="">
+              <div>
                 <h1 className="text-6xl tracking-tight font-bold font-logo text-gray-900 leading-none">
                   V1
                 </h1>
                 <h2 className="text-2xl tracking-tight font-bold italic text-gradient bg-gradient-to-r from-yellow-600 to-yellow-700">
                   at Michigan
                 </h2>
-                <p className="text-2xl mt-8 text-gray-900 font-bold tracking-tight mb-3">
+                <p className="text-2xl mt-6 text-gray-900 font-bold tracking-tight mb-3">
                   The community for ambitious student builders at the University
                   of Michigan.
                 </p>
+                <div className="block">
+                  <Link href="/join" passHref>
+                    <button
+                      type="button"
+                      className="bg-gradient-to-r from-yellow-600 to-yellow-700 hover:bg-blue-500 text-gray-100 font-semibold py-3 px-4 rounded shadow mt-5 hover:opacity-75"
+                    >
+                      Sign up &rsaquo;
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
             <div className="max-w-xl p-4">
