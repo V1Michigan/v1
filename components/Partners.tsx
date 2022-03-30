606877689ca38c6b4d2b939eca74a6b3415b8e2b
Elliot Klein <elliotfklein@gmail.com>
2022-03-30 19:28:04 -0400

    Fix image colors + sizing

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -2,9 +2,14 @@ import Fade from "./Fade";
 
 const PARTNERS = [
   {
-    name: "Y Combinator",
-    link: "https://ycombinator.com",
-    image: "partners/yc.png",
+    name: "Ramp",
+    link: "https://ramp.com/",
+    image: "/partners/ramp.svg",
+  },
+  {
+    name: "Neo",
+    link: "https://neo.com/",
+    image: "/partners/neo.png",
   },
   {
     name: "Contrary",
@@ -12,44 +17,36 @@ const PARTNERS = [
     image: "/partners/contrary.svg",
   },
   {
-    name: "Neo",
-    link: "https://neo.com/",
-    image: "/partners/Neo.png",
+    name: "Y Combinator",
+    link: "https://ycombinator.com",
+    image: "/partners/yc.png",
   },
   {
     name: "Cahoots",
     link: "https://cahoots.com/",
-    image: "/partners/Cahoots.png",
-  },
-  {
-    name: "Ramp",
-    link: "https://ramp.com/",
-    image: "/partners/ramp.svg",
+    image: "/partners/cahoots.png",
   },
 ];
 
 const Partners = () => (
-  <Fade motion={false}>
-    <div>
-      <h2 className="flex items-center justify-around text-4xl md:text-6xl text-black font-bold py-6">
-        Partners
-      </h2>
-      <p className="tracking-tight text-gray-900 text-3xl max-w-4xl mx-auto py-2 px-4 leading-snug">
-        We partner with outstanding organizations to create extraordinary
-        projects and connect our members with exclusive opportunities.
-      </p>
-      <div className="flex items-center justify-around">
+  <div>
+    <h2 className="text-center text-3xl md:text-5xl text-black font-bold py-6">
+      Partners
+    </h2>
+    <p className="tracking-tight text-gray-900 text-2xl max-w-4xl mx-auto py-2 px-4 leading-snug">
+      We partner with outstanding organizations to build extraordinary projects
+      and connect our members with exclusive opportunities.
+    </p>
+    <Fade motion={false}>
+      <div className="max-w-5xl mx-auto p-8 pb-12 flex flex-wrap items-center justify-around gap-x-12 gap-y-8">
         {PARTNERS.map(({ name, link, image }) => (
-          <a href={link}>
-            <p className="justify-around md:text-2xl text-black font-semibold">
-              {name}
-            </p>
-            <img alt={name} src={image} />
+          <a className="shrink-0" href={link}>
+            <img className="h-8" alt={name} src={image} />
           </a>
         ))}
       </div>
-    </div>
-  </Fade>
+    </Fade>
+  </div>
 );
 
 export default Partners;
