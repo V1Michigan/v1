8d54c01338c6a64c2d8f8681cd33b71b1146b8d1
Nathan Newman <nathanieljnewman42@gmail.com>
2022-03-30 16:26:52 -0400

    Add partner logos. Style text.

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -1,24 +1,55 @@
+import Fade from "./Fade";
+
 const PARTNERS = [
   {
     name: "Y Combinator",
     link: "https://ycombinator.com",
-    image: "/partners/yc.png",
+    image: "partners/yc.png",
+  },
+  {
+    name: "Contrary",
+    link: "https://contrary.com/",
+    image: "/partners/contrary.svg",
+  },
+  {
+    name: "Neo",
+    link: "https://neo.com/",
+    image: "/partners/Neo.png",
+  },
+  {
+    name: "Cahoots",
+    link: "https://cahoots.com/",
+    image: "/partners/Cahoots.png",
+  },
+  {
+    name: "Ramp",
+    link: "https://ramp.com/",
+    image: "/partners/ramp.svg",
   },
 ];
 
 const Partners = () => (
-  <div>
-    <h2>Partners</h2>
-    <p>
-      We partner with outstanding organizations to create extraordinary projects
-      and connect our members with exclusive opportunities.
-    </p>
+  <Fade motion={false}>
     <div>
-      {PARTNERS.map(({ name, link, image }) => (
-        <p>{name}</p>
-      ))}
+      <h2 className="flex items-center justify-around text-4xl md:text-6xl text-black font-bold py-6">
+        Partners
+      </h2>
+      <p className="tracking-tight text-gray-900 text-3xl max-w-4xl mx-auto py-2 px-4 leading-snug">
+        We partner with outstanding organizations to create extraordinary
+        projects and connect our members with exclusive opportunities.
+      </p>
+      <div className="flex items-center justify-around">
+        {PARTNERS.map(({ name, link, image }) => (
+          <a href={link}>
+            <p className="justify-around md:text-2xl text-black font-semibold">
+              {name}
+            </p>
+            <img alt={name} src={image} />
+          </a>
+        ))}
+      </div>
     </div>
-  </div>
+  </Fade>
 );
 
 export default Partners;
