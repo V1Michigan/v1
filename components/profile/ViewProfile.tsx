12bad58a596f598a4ec11a596cf4987510d5a613
Elliot Klein <elliotfklein@gmail.com>
2022-04-03 13:31:57 -0400

    Allow missing fields on profile pages

--- components/profile/ViewProfile.tsx
+++ components/profile/ViewProfile.tsx
@@ -26,39 +26,55 @@ const ViewProfile = ({ profile }: ViewProfileProps) => (
         </a>
       )}
     </div>
-    {/* If "additional links" is a single URL, link to that; else, just show the text */}
-    {/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)$/.test(
-      profile.website
-    ) ? (
-      <a href={profile.website} target="_blank" rel="noopener noreferrer">
-        {profile.website}
-      </a>
-    ) : (
-      <p>{profile.website}</p>
+    {profile.website && (
+      <>
+        {/* If "additional links" is a single URL, link to that; else, just show the text */}
+        {/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)$/.test(
+          profile.website
+        ) ? (
+          <a href={profile.website} target="_blank" rel="noopener noreferrer">
+            {profile.website}
+          </a>
+        ) : (
+          <p>{profile.website}</p>
+        )}
+      </>
     )}
-    <p className="my-4">{profile.bio}</p>
-    <p>Year: {Year[profile.year]}</p>
-    <p>
-      Major
-      {profile.fields_of_study.majors.length > 1 && "s"}:{" "}
-      {profile.fields_of_study.majors
-        .map((majorKey) => FieldOfStudy[majorKey])
-        .join(", ")}
-    </p>
-    {profile.fields_of_study.minors.length > 0 && (
+    {profile.bio && <p className="my-4">{profile.bio}</p>}
+    {profile.year && <p>Year: {Year[profile.year]}</p>}
+    {profile.fields_of_study && (
+      <>
+        <p>
+          Major
+          {profile.fields_of_study.majors.length > 1 && "s"}:{" "}
+          {profile.fields_of_study.majors
+            .map((majorKey) => FieldOfStudy[majorKey])
+            .join(", ")}
+        </p>
+        {profile.fields_of_study.minors?.length > 0 && (
+          <p>
+            Minor
+            {profile.fields_of_study.minors.length > 1 && "s"}:{" "}
+            {profile.fields_of_study.minors
+              .map((minorKey) => FieldOfStudy[minorKey])
+              .join(", ")}
+          </p>
+        )}
+      </>
+    )}
+    {profile.roles && (
       <p>
-        Minor
-        {profile.fields_of_study.minors.length > 1 && "s"}:{" "}
-        {profile.fields_of_study.minors
-          .map((minorKey) => FieldOfStudy[minorKey])
+        Roles: {profile.roles.map((roleKey) => RoleType[roleKey]).join(", ")}
+      </p>
+    )}
+    {profile.interests && (
+      <p>
+        Interests:{" "}
+        {profile.interests
+          .map((interestKey) => Interest[interestKey])
           .join(", ")}
       </p>
     )}
-    <p>Roles: {profile.roles.map((roleKey) => RoleType[roleKey]).join(", ")}</p>
-    <p>
-      Interests:{" "}
-      {profile.interests.map((interestKey) => Interest[interestKey]).join(", ")}
-    </p>
   </div>
 );
 
