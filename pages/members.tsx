2f3119c4a145c24e588fcbaab0dd163b399c4059
Elliot Klein <elliotfklein@gmail.com>
2022-04-10 13:32:31 -0400

    fix: remove color from /members 'Back' button

--- pages/members.tsx
+++ pages/members.tsx
@@ -273,9 +273,10 @@ const Members: NextPage = () => {
       <div className="flex w-full justify-center items-center">
         <InternalLink href="/dashboard" className="justify-self-start">
           <button
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow text-sm font-medium rounded-md
-              text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
-              disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-indigo-600"
+            className="inline-flex justify-center py-2 px-4 border border-black shadow text-sm font-medium rounded-md
+              text-black hover:bg-white-700 hover:bg-gray-100
+              focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black-500
+              disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-white-600"
             type="button"
           >
             Back
