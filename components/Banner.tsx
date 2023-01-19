f511c12aa27a27f1957280fa5ff51c66ca664d37
Shrey Pandya <shrey150@yahoo.com>
2023-01-19 12:56:20 -0500

    ensure banner button is clickable

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -24,7 +24,7 @@ const Banner = ({ text, link, buttonText }: BannerProps) => {
         <p className="p-3 flex-1 flex-grow font-medium font-sans text-white">
           {text}
         </p>
-        <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+        <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto z-50">
           <a
             href={link}
             className="font-sans flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-yellow-600 bg-white hover:bg-yellow-200 transition duration-500"
