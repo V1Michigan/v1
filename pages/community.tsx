479ec33bedaabd1102fdb4f56875646eacd6037a
RaghavBhat02 <bhatr@umich.edu>
2023-01-20 01:25:28 -0500

    community link updates slack info in db + redirects to slack

---
+++ pages/community.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import useSupabase from "../hooks/useSupabase";
+import Head from "../components/Head";
+import ProtectedRoute from "../components/ProtectedRoute";
+
+const SendSlack: NextPage = () => {
+  const { supabase, user } = useSupabase();
+  supabase
+    .from("slack")
+    .insert({ user_id: user?.id })
+    .then(({ error }) => {
+      // eslint-disable-next-line no-console
+      if (error?.code !== "23505") console.log(error);
+    });
+  const router = useRouter();
+  router.push(
+    "https://join.slack.com/t/v1community/shared_invite/zt-1namcs482-XjoRlPYk_MQX71nCGFxjWA"
+  );
+
+  return (
+    <>
+      <Head title="Slack..." />
+      <p>Redirecting to Slack...</p>
+    </>
+  );
+};
+const ProtectedSendSlack = () => (
+  <ProtectedRoute>
+    <SendSlack />
+  </ProtectedRoute>
+);
+
+export default ProtectedSendSlack;
