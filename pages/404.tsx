f5ad24872eca1d871d96d3e2f77fa4bc3cdbaa96
Deric Dinu Daniel <33469382+dericdinudaniel@users.noreply.github.com>
2023-02-19 17:09:09 -0500

    ddd 404 page PLA-33 (#60)
    * basic 404 page
    
    * added comments
    
    Note for future reference: update this to look more interesting in the next version. 
    ---------
    
    Co-authored-by: Deric Dinu Daniel <dericdinudaniel@Derics-MacBook-Pro.local>

---
+++ pages/404.tsx
@@ -0,0 +1,33 @@
+// 404.js
+import { useEffect } from "react";
+import InternalLink from "../components/Link";
+
+export default function Custom404() {
+  // adds bg-gray-800 to body on mount and removes it on unmount
+  // i wanted to make area outside viewport colored (when you scroll up or down past the page)
+  useEffect(() => {
+    document.body.classList.add("bg-gray-800");
+    return () => {
+      document.body.classList.remove("bg-gray-800");
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="h-screen flex flex-col">
+        <div className="absolute top-0 left-0 w-full bg-gray-800 flex justify-center flex-grow pt-16">
+          <InternalLink href="/">
+            <img className="w-20" src="/v1logowhite.svg" alt="V1 Logo" />
+          </InternalLink>
+        </div>
+        <div className="h-screen flex flex-col items-center justify-center bg-gray-800">
+          <h1 className="text-8xl font-bold text-gray-200 flex items-center">
+            <span>404</span>
+            <span className="border-l mx-4 h-full" />
+            <span className="text-gray-200 text-2xl">Page not found</span>
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
