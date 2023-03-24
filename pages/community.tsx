ba28484644e8c56e623ae2c08b2559fe009f6061
Shrey Pandya <shrey150@yahoo.com>
2023-03-24 15:07:28 -0400

    [sp] remove protected route for Community Slack (#69)
    * remove protected route for Community Slack
    
    * [sp] update Slack link & fix next router issue
    
    * [sp] only track Slack clicks if user is logged in

--- pages/community.tsx
+++ pages/community.tsx
@@ -1,25 +1,32 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import useSupabase from "../hooks/useSupabase";
 import Head from "../components/Head";
-import ProtectedRoute from "../components/ProtectedRoute";
 
 const SendSlack: NextPage = () => {
   const { supabase, user } = useSupabase();
-  supabase
-    .from("slack")
-    .insert({ user_id: user?.id })
-    .then(({ error }) => {
-      // following error (in the if statement) is when supabase can't add someone
-      // to the table, which is okay!! This just means they've clicked on the link
-      // before. Therefore, we ignore the error.
-      // eslint-disable-next-line no-console
-      if (error?.code !== "23505") console.log(error);
-    });
   const router = useRouter();
-  router.push(
-    "https://join.slack.com/t/v1community/shared_invite/zt-1namcs482-XjoRlPYk_MQX71nCGFxjWA"
-  );
+
+  // only track Slack analytics if user is logged in
+  if (user) {
+    supabase
+      .from("slack")
+      .insert({ user_id: user?.id })
+      .then(({ error }) => {
+        // following error (in the if statement) is when supabase can't add someone
+        // to the table, which is okay!! This just means they've clicked on the link
+        // before. Therefore, we ignore the error.
+        // eslint-disable-next-line no-console
+        if (error?.code !== "23505") console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    router.push(
+      "https://join.slack.com/t/v1community/shared_invite/zt-1rux4efyc-STBoY07IAKYpCODM5ls5~g"
+    );
+  }, [router]);
 
   return (
     <>
@@ -28,10 +35,5 @@ const SendSlack: NextPage = () => {
     </>
   );
 };
-const ProtectedSendSlack = () => (
-  <ProtectedRoute>
-    <SendSlack />
-  </ProtectedRoute>
-);
 
-export default ProtectedSendSlack;
+export default SendSlack;
