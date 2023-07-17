441e6d2656078623a63b3965c4a219925e6dd505
Shrey Pandya <shrey150@yahoo.com>
2023-07-17 02:21:34 -0400

    [sp] show custom error message

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import InternalLink from "../components/Link";
 import ErrorButtons from "../components/ErrorButtons";
 
 // modified from 404.tsx
 export default function Custom500() {
+  const router = useRouter();
+  const { msg } = router.query;
+
   // adds bg-gray-800 to body on mount and removes it on unmount
   // i wanted to make area outside viewport colored (when you scroll up or down past the page)
   useEffect(() => {
@@ -23,8 +27,13 @@ export default function Custom500() {
         </div>
         <div className="h-screen flex flex-col items-center justify-center bg-gray-800">
           <h1 className="text-6xl font-bold text-gray-200 flex items-center">
-            <span>Unexpected error :(</span>
+            Unexpected error occurred :(
           </h1>
+          {msg && (
+            <p className="text-2xl text-gray-200 font-mono mt-8">
+              <code className="text-gray-200">{msg}</code>
+            </p>
+          )}
           <ErrorButtons />
         </div>
       </div>
