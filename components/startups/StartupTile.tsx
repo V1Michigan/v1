1e96472158ef1395a1c844343009e289392ffc2d
Brian Travis <bztravis@umich.edu>
2023-07-23 17:36:56 -0400

    Update misc startup tile styling for better sync w/ figma

--- components/startups/StartupTile.tsx
+++ components/startups/StartupTile.tsx
@@ -18,45 +18,48 @@ export default function StartupTile({
   websiteLink,
 }: StartupTileProps) {
   return (
-    <div className="flex flex-col bg-white font-bold leading-none text-gray-800 uppercase rounded-lg shadow-lg duration-100 border border-stone-300">
-      <img
-        src={logo}
-        className="w-1/2 mx-auto mt-6 mb-1 rounded-lg"
-        alt="logo"
-      />
+    <div className="flex flex-col bg-white font-bold p-6 leading-none text-gray-800 uppercase rounded-lg shadow-lg duration-100 border border-stone-300">
+      <img src={logo} className="w-1/2 mx-auto rounded-lg" alt="logo" />
 
-      <div className="mt-3 mb-2 mx-auto text-center">
+      <div className="mt-6 mx-auto text-center">
         <h1 className="normal-case text-xl font-semibold leading-6 truncate">
           {name}
         </h1>
       </div>
 
-      <p className="mx-4 normal-case text-base font-normal leading-5 tracking-normal text-left mb-3 text-gray-500 truncate text-ellipsis">
+      <p
+        style={{ height: "60px", overflow: "hidden", textOverflow: "ellipsis" }}
+        className="normal-case text-base font-normal leading-5 tracking-normal text-left mt-3 text-gray-500"
+      >
         {description}
       </p>
 
-      <div className="flex my-3 mt-auto justify-center items-center">
+      <div className="flex mt-4 justify-between items-center">
         <button
           type="button"
-          style={{ backgroundColor: "#212936" }}
-          className="rounded-lg p-2 font-inter text-sm leading-5 tracking-normal text-left text-gray-200 mx-1 "
+          style={{
+            backgroundColor: "#212936",
+            width: "calc(50% - 6px)",
+          }}
+          className="rounded-lg p-2 font-inter text-sm leading-5 tracking-normal text-left text-gray-200 flex justify-center"
         >
-          <a href={websiteLink} className="flex flex-row m-auto items-center">
-            <InformationCircleIcon className=" inline-block h-4 w-4" />
-            <p className="inline-block ml-1">See More</p>
+          <a href={websiteLink} className="flex flex-row m-auto items-center gap-1">
+            <InformationCircleIcon className=" inline-block h-5 w-5" />
+            <p className="inline-block">See More</p>
           </a>
         </button>
 
         <button
           type="button"
-          className="rounded-lg p-2 font-inter text-sm leading-5 tracking-normal text-left text-gray-600 bg-gray-300 mx-1 "
+          style={{ width: "calc(50% - 6px)", }}
+          className="rounded-lg p-2 font-inter text-sm leading-5 tracking-normal text-left text-gray-600 bg-gray-300 flex justify-center"
         >
-          <a href={websiteLink} className="flex flex-row m-auto items-center">
-            <ExternalLinkIcon className=" inline-block h-4 w-4" />
-            <p className="inline-block ml-1">Website</p>
+          <a href={websiteLink} className="flex flex-row m-auto items-center gap-1">
+            <ExternalLinkIcon className=" inline-block h-5 w-5" />
+            <p className="inline-block">Website</p>
           </a>
         </button>
       </div>
-    </div>
+    </div >
   );
 }
