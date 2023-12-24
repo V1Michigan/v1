517fceea116515aa226ab9a1f63930a9df4ef073
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-12-24 17:29:18 -0500

    Add team position links (#84)
    * team positions redirect
    
    * added redirections for /team and /team/apply

---
+++ pages/team/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Redirect from "../../components/Redirect";
+
+const TeamPage: NextPage = () => (
+  <Redirect route="https://v1team.notion.site/Join-the-V1-Team-45b77563d6bb44a0bd343fc73409112a" />
+);
+
+export default TeamPage;
