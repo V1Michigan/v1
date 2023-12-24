d9bd6793ca5a7c0eaa56297ddcaf6bfc2b068c96
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-12-24 17:10:35 -0500

    added redirections for /team and /team/apply

---
+++ pages/team/apply.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Redirect from "../../components/Redirect";
+
+const TeamApplication: NextPage = () => (
+  <Redirect route="https://tally.so/r/3X4VXj" />
+);
+
+export default TeamApplication;
--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import Redirect from "../components/Redirect";
+import Redirect from "../../components/Redirect";
 
 const TeamPage: NextPage = () => (
   <Redirect route="https://v1team.notion.site/Join-the-V1-Team-45b77563d6bb44a0bd343fc73409112a" />
