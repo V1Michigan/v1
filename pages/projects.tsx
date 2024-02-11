b1d788e76c7438837c904b63216dc03485fa10ad
Connor Park <connorpark024@gmail.com>
2024-02-11 16:39:04 -0500

    add projects route

---
+++ pages/projects.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import Head from "../components/Head";
+import NavbarBuilder from "../components/NavBar";
+import DirectoryLayout from "../components/DirectoryLayout";
+
+const Projects: NextPage = () => (
+  <>
+    <Head title="Projects" />
+    <NavbarBuilder />
+    <DirectoryLayout
+      title="Projects"
+      description="Under construction! Check back soon."
+      link="https://www.google.com"
+    />
+  </>
+);
+
+export default Projects;
