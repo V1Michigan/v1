aa360d25e6754fee3bd6ca2c1d6ee96e6acdca54
arael <jonasg@umich.edu>
2024-02-11 17:48:12 -0500

    connect dialog styling, authwall, building API request.

--- components/startups/StartupProfileTile.tsx
+++ components/startups/StartupProfileTile.tsx
@@ -1,6 +1,12 @@
 import { useState, Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import supabase from "../../utils/supabaseClient";
 import { StartupProfile, StartupProfileMetadata } from "../../utils/types";
+
+const CONNECTION_REQUEST_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/connection"
+    : "https://v1-api-production.up.railway.app/connection";
 
 export default function StartupProfileTile({
   startupProfile,
@@ -10,11 +16,40 @@ export default function StartupProfileTile({
   startupProfileMetadata: StartupProfileMetadata;
 }) {
   const { username, name } = startupProfile;
+  const recipient = name ?? username;
   const { role, headshot_src: headshotSrc } = startupProfileMetadata;
   const [connectDialogOpen, setConnectDialogOpen] = useState<boolean>(false);
+  const [connectionMessage, setConnectionMessage] = useState<string>("");
+  const session = supabase.auth.session();
 
   const anonymousPersonImage =
     "https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg";
+
+  function sendConnectionMessage() {
+    // Null handling.
+    if (!session) {
+      return;
+    }
+    const { access_token: accessToken } = session;
+
+    const body = JSON.stringify({ message: connectionMessage, recipient });
+    fetch(CONNECTION_REQUEST_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body,
+    });
+    /* TODO (jonas): response handling, CORS, testing, etc.
+      .then((response) => {
+        if (response.ok) {
+          setConnectDialogOpen(false);
+        }
+      })
+      .catch((error) => {});
+      */
+  }
 
   return (
     <div className="flex flex-col items-center mr-5 w-36 mb-5">
@@ -23,16 +58,18 @@ export default function StartupProfileTile({
         src={headshotSrc ?? anonymousPersonImage}
         height={80}
         width={80}
-        alt={`${username} headshot`}
+        alt={`${recipient} headshot`}
       />
-      <h1 className="mt-1">{name ?? username}</h1>
+      <h1 className="mt-1">{name ?? recipient}</h1>
       <p className="text-gray-500 text-xs">{role}</p>
       <button
         type="button"
         style={{
           backgroundColor: "#212936",
         }}
-        className="text-xs rounded bg-slate-300 px-2 py-1 mt-2 font-inter text-gray-200"
+        className={`text-xs rounded px-2 py-1 mt-2 font-inter text-gray-200 ${
+          connectDialogOpen ? "hidden" : ""
+        }`}
         onClick={() => setConnectDialogOpen(true)}
       >
         Connect
@@ -65,11 +102,29 @@ export default function StartupProfileTile({
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <div>
-                  <input type="text" placeholder="Write a message..." />
-                  <button type="submit">Send🚀</button>
-                </div>
+              <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white px-6 py-2 text-left align-middle shadow-xl transition-all">
+                {session ? (
+                  <div className="flex justify-between items-center">
+                    <input
+                      value={connectionMessage}
+                      onChange={(evt) => setConnectionMessage(evt.target.value)}
+                      className="w-full text-sm border border-gray-400 border-1 rounded h-fit"
+                      type="text"
+                      placeholder="Write a message..."
+                    />
+                    <button
+                      className="text-sm text-gray-400 p-4"
+                      type="submit"
+                      onClick={sendConnectionMessage}
+                    >
+                      Send
+                    </button>
+                  </div>
+                ) : (
+                  <p className="text-sm text-gray-400">
+                    Sign in to connect with {recipient}!
+                  </p>
+                )}
               </Dialog.Panel>
             </Transition.Child>
           </div>
