0b4d8ef0c8b2b8fa1b34d952533543076adb4a62
Shrey Pandya <shrey150@yahoo.com>
2025-03-03 19:35:32 -0500

    add posthog analytics (again) (#123)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import ReactGA from "react-ga4";
 import type { AppProps } from "next/app";
 import PropTypes from "prop-types";
 import { QueryClient, QueryClientProvider } from "react-query";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { SupabaseProvider } from "../contexts/SupabaseContext";
 import "../styles/index.css";
 
@@ -13,23 +15,42 @@ export const HOSTNAME =
     ? "http://localhost:3000"
     : process.env.NEXT_PUBLIC_HOSTNAME || "https://v1michigan.com";
 
+// Initialize PostHog on the client side only
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+    api_host:
+      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only",
+    loaded: (_posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+    capture_pageview: true, // Make sure pageview capturing is enabled
+  });
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
-  useEffect(
-    // "V1 website" property automatically tracks some events, e.g. page views
-    () => {
-      if (process.env.NODE_ENV !== "development") {
-        ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID || "");
-      }
-    },
-    []
-  );
+
+  useEffect(() => {
+    // Test PostHog connection
+    if (typeof window !== "undefined") {
+      posthog.capture("app_loaded");
+    }
+
+    // Existing GA code
+    if (process.env.NODE_ENV !== "development") {
+      ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID || "");
+    }
+  }, []);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <SupabaseProvider>
-        <Component {...pageProps} />
-      </SupabaseProvider>
-    </QueryClientProvider>
+    <PostHogProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        <SupabaseProvider>
+          <Component {...pageProps} />
+        </SupabaseProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
 
